db192e7f923d99279204d905ce1e526c93ad74d1
Gur <leviavgurarie@gmail.com>
2023-03-25 22:21:22 +0300

    added sub and add vec in point

--- ISE5783_9890_4580/src/primitives/Point.java
+++ ISE5783_9890_4580/src/primitives/Point.java
@@ -1,2 +1,65 @@
-package primitives;public class Point {
+package primitives;
+
+public class Point {
+    final protected Double3 xyz;
+
+    public static final Point ZERO = new Point(0, 0, 0);
+
+    /**
+     * Constructor to initialize Point based object with its three number values
+     *
+     * @param x first number value
+     * @param y second number value
+     * @param z third number value
+     */
+    public Point(double x, double y, double z) {
+        this.xyz = new Double3(x, y, z);
+    }
+
+    /**
+     * Constructor to initialize Point based object with its Double3 object
+     *
+     * @param xyz the Double3 object
+     */
+    protected Point(Double3 xyz) {
+        this.xyz = new Double3(xyz.d1, xyz.d2, xyz.d3);//maybe assign this.xyz = xyz
+    }
+
+    public double getX() {
+        return xyz.d1;
+    }
+    /**
+     * Subtract two Point object into a new Point where each couple of
+     * numbers is subtracted
+     *
+     * @param other right handle side operand for addition
+     * @return result of subtract
+     */
+    public Vector subtract(Point other) {
+        return new Vector(this.xyz.subtract(other.xyz));
+    }
+
+    /**
+     * Subtract two Point object into a new Point where each couple of
+     * numbers is added
+     *
+     * @param other right handle side operand for addition
+     * @return result of add
+     */
+    public Point add(Vector other) {
+        return new Point(this.xyz.add(other.xyz));
+    }
+
+    /**
+     * find the distance squared of two Point objects
+     *
+     * @param other the other Point to find distance squared from
+     * @return distance between Points squared
+     */
+    public double distanceSquared(Point other) {
+        double dX = xyz.d1 - other.xyz.d1;
+        double dY = xyz.d2 - other.xyz.d2;
+        double dZ = xyz.d3 - other.xyz.d3;
+        return dX * dX + dY * dY + dZ * dZ;
+    }
 }
--- ISE5783_9890_4580/src/primitives/Util.java
+++ ISE5783_9890_4580/src/primitives/Util.java
@@ -1,4 +1,4 @@
-package geometries;
+package primitives;
 
 /**
  * Util class is used for some internal utilities, e.g. controlling accuracy
